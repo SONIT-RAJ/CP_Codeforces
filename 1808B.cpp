bf6472e1ed028589759bb7f845a18f14cab2d7db
SONIT-RAJ <sonitraj8458@gmail.com>
2025-03-22 22:32:39 +0530

    add some snippets

--- 1808B.cpp
+++ 1808B.cpp
@@ -1,12 +1,41 @@
+/*
+   Author: SONIT RAJ
+    created: 22:29:27 22-03-2025
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    long long t;
+#define ll long long
+#define int long long
+#define rep(i,a,b) for(int i = a; i<b; i++)
+#define rew(x) for(int i = 0; i<x; i++)
+#define all(x) x.begin(), x.end()
+#ifdef ONLINE_JUDGE
+    #define de(...)
+    #define de2(...)
+#endif
+const ll inf = 2e18 + 5;
+const ll M = 1e9 + 7;
+#define PI 3.141592653589
+
+void solve(){
+
+    int n;
+    cin>>n;
+
+
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
     cin>>t;
     while(t--){
-        long long n,m;
-        cin>>n>>m;
-
+        solve();
+        cout<<"\n";
     }
-    return 0;
 }
