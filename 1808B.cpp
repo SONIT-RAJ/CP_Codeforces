a23acd57e1339e293321815e6576c5148c60536c
SONIT-RAJ <sonitraj8458@gmail.com>
2025-03-24 10:29:56 +0530

    add new question

--- 1808B.cpp
+++ 1808B.cpp
@@ -1,6 +1,6 @@
 /*
    Author: SONIT RAJ
-    created: 00:05:58 23-03-2025
+    created: 22:00:28 23-03-2025
 */
 
 
@@ -25,8 +25,37 @@ const ll M = 1e9 + 7;
 
 void solve(){
 
-    int n;
-    cin>>n;
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>a(m,vector<int>(n,0));
+    vector<vector<int>>b(m,vector<int>(n,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[j][i];
+        }
+    }
+    for (auto &row : a) {
+        sort(row.begin(), row.end());
+    }
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            b[i][j]=a[i][j];
+            if(j!=0){
+                b[i][j]+=b[i][j-1];
+            }
+        }
+    }
+    int sum = 0;
+    for (int j = 0; j < m; j++) {
+        int prefix_sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[j][i] * i - prefix_sum;  // Difference sum calculation
+            prefix_sum += a[j][i];  // Update prefix sum
+        }
+    }
+
+    cout << sum;
+
 
 
 
