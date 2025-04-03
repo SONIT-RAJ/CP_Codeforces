e2565cad9cb305210ff1121114bbe10934732798
SONIT-RAJ <sonitraj8458@gmail.com>
2025-04-04 00:15:38 +0530

    add new question

---
+++ 2091C.cpp
@@ -0,0 +1,52 @@
+/*
+   Author: SONIT RAJ
+    created: 00:13:42 04-04-2025
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define int long long
+#define rep(i,a,b) for(int i = a; i<b; i++)
+#define rew(x) for(int i = 0; i<x; i++)
+#define all(x) x.begin(), x.end()
+#ifdef ONLINE_JUDGE
+    #define de(...)
+    #define de2(...)
+#endif
+const ll inf = 2e18 + 5;
+const ll M = 1e9 + 7;
+#define PI 3.141592653589
+
+void solve(){
+
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<-1;
+    }
+    else{
+        for(int i=n;i>=1;i--){
+            cout<<i<<" ";
+        }
+    }
+
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+}
