3b651847a479a88480b7909ef2a5430bc1e8093f
SONIT-RAJ <sonitraj8458@gmail.com>
2025-05-28 10:52:53 +0530

    add new question

--- 2102D.cpp
+++ 2102D.cpp
@@ -21,6 +21,88 @@ void solve(){
 
     int n;
     cin>>n;
+    if(n==4){
+        vector<int>a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int n1=a[0]*1000+a[1]*100+a[2]*10+a[3];
+        int n2=a[2]*1000+a[3]*100+a[0]*10+a[1];
+        if(n1<=n2){
+            cout<<a[0]<<" "<<a[1]<<" "<<a[2]<<" "<<a[3];
+            return;
+        }
+        else{
+            cout<<a[2]<<" "<<a[3]<<" "<<a[0]<<" "<<a[1];
+            return;
+        }
+
+    }
+    vector<int>e,e1;
+    vector<int>o,o1;
+    e.reserve(n);
+    o.reserve(n);
+    int x;
+    for(int i=1;i<=n;i++){
+        cin>>x;
+        if(i%2==0){
+            e.emplace_back(x);
+        }
+        else{
+            o.emplace_back(x);
+        }
+    }
+    e1=e;
+    o1=o;
+    sort(e.begin(),e.end());
+    sort(o.begin(),o.end());
+    int k=0;
+    int l=0;
+    map<int,bool>mpp;
+    for(int i=1;i<=n-3;i++){
+        if(i%2==1){
+            cout<<o[k]<<" ";
+            mpp[o[k]]=true;
+            k++;
+        }
+        else{
+            cout<<e[l]<<" ";
+            mpp[e[l]]=true;
+            l++;
+        }
+    }
+    if(n%2==0){
+        int flag
+        for(int i=0;i<e1.size();i++){
+            if(mpp[e1[i]]==true){
+                continue;
+            }
+            cout<<e1[i]<<" ";
+            for(int j=0;j<o1.size();j++){
+                if(mpp[o1[j]]==true){
+                    continue;
+                }
+                cout<<o1[j]<<" ";
+            }
+        }
+    }
+    else{
+        for(int i=0;i<o1.size();i++){
+            if(mpp[o1[i]]==true){
+                continue;
+            }
+            cout<<o1[i]<<" ";
+            for(int j=0;j<e1.size();j++){
+                if(mpp[e1[j]]==true){
+                    continue;
+                }
+                cout<<e1[j]<<" ";
+            }
+        }
+    }
+
+
+
 
 
 }
