cbe7ef00880b74a687c01c23e7ebeb149ba49c90
SONIT-RAJ <sonitraj8458@gmail.com>
2025-07-28 14:46:06 +0530

    add new question

---
+++ 2128B.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define int long long
+#define rep(i,a,b) for(int i = a; i<b; i++)
+#define rew(x) for(int i = 0; i<x; i++)
+#define all(x) x.begin(), x.end()
+#ifdef ONLINE_JUDGE
+    #define de(...)
+    #define de2(...)
+#endif
+const ll inf = 2e18 + 5;
+const ll M = 1e9 + 7;
+#define PI 3.141592653589
+
+void solve(){
+
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int i=1;
+    int j=n-1;
+    int flag=0;
+    int wrong=1;
+    int last=a[0];
+    string ans;
+    ans1.push_back('L');
+    while(i<=j){
+        char temp;
+        if(flag==0){
+            if(a[i]>a[j]){
+                temp='L';
+            }
+            else{
+                temp='R';
+            }
+            flag=1;
+        }
+        else{
+            if(a[i]<a[j]){
+                temp='R';
+            }
+            else{
+                temp='L';
+            }
+            flag=0;
+        }
+    }
+
+
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+}
