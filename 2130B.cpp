86e0d9170b1d0a7256a28ccbe0e2e0cbe1ee3919
SONIT-RAJ <sonitraj8458@gmail.com>
2025-08-01 22:14:03 +0530

    add new question

--- 2130B.cpp
+++ 2130B.cpp
@@ -1,4 +1,10 @@
-#include <bits/stdc++.h>
+/*
+   Author: SONIT RAJ
+    created: 20:14:07 31-07-2025
+*/
+
+
+#include<bits/stdc++.h>
 using namespace std;
 
 #pragma GCC optimize("Ofast,unroll-loops")
@@ -6,56 +12,63 @@ using namespace std;
 
 #define ll long long
 #define int long long
-#define rep(i,a,b) for(int i = a; i < b; i++)
-#define rew(x) for(int i = 0; i < x; i++)
+#define rep(i,a,b) for(int i = a; i<b; i++)
+#define rew(x) for(int i = 0; i<x; i++)
 #define all(x) x.begin(), x.end()
 #ifdef ONLINE_JUDGE
     #define de(...)
     #define de2(...)
 #endif
-
 const ll inf = 2e18 + 5;
 const ll M = 1e9 + 7;
 #define PI 3.141592653589
 
-void solve() {
-    int n, s;
-    cin >> n >> s;
-    vector<int> a(n);
-    for (int &x : a) cin >> x;
+void solve(){
 
-    ll total = accumulate(all(a), 0LL);
-
-    if (s < total || s == total + 1) {
-        int c0 = 0, c1 = 0, c2 = 0;
-        for (int x : a) {
-            if (x == 0) ++c0;
-            else if (x == 1) ++c1;
-            else ++c2;
+    int n,s;
+    cin>>n>>s;
+    vector<int>a(n);
+    int sum=0;
+    int t=0;
+    int z=0;
+    int o=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        sum+=a[i];
+        if(a[i]==2)t++;
+        if(a[i]==0)z++;
+        if(a[i]==1)o++;
+    }
+    if(sum==s){
+        cout<<-1;
+        return;
+    }
+    if(sum>s){
+        for(int i=0;i<n;i++){
+            cout<<a[i]<<" ";
         }
-        bool first = true;
-        while (c0--) { cout << (first ? "" : " ") << 0; first = false; }
-        while (c2--) { cout << (first ? "" : " ") << 2; first = false; }
-        while (c1--) { cout << (first ? "" : " ") << 1; first = false; }
-    } else {
-        cout << -1;
+        return;
     }
+    if(sum+1==s){
+        for(int i=0;i<z;i++)cout<<0<<" ";
+        for(int i=0;i<t;i++)cout<<2<<" ";
+        for(int i=0;i<o;i++)cout<<1<<" ";
+
+        return;
+    }
+    cout<<-1;
+
+
+
 }
 
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int T;
-    cin >> T;
-    while (T--) {
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
         solve();
-        cout << '\n';
+        cout<<"\n";
     }
-    return 0;
 }
-
-/*
-   Author: SONIT RAJ
-   created: 21:41:48 31-07-2025
-*/
