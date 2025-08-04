63fcdbbfa985c403c7209e88b9120fed0647a6a3
SONIT-RAJ <sonitraj8458@gmail.com>
2025-08-04 13:59:21 +0530

    add new question

---
+++ 1316E.cpp
@@ -0,0 +1,68 @@
+/*
+   Author: SONIT RAJ
+    created: 13:30:29 04-08-2025
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define int long long
+#define rep(i,a,b) for(int i = a; i<b; i++)
+#define rew(x) for(int i = 0; i<x; i++)
+#define all(x) x.begin(), x.end()
+#ifdef ONLINE_JUDGE
+    #define de(...)
+    #define de2(...)
+#endif
+const ll inf = 2e18 + 5;
+const ll M = 1e9 + 7;
+#define PI 3.141592653589
+
+int f(int i,int k,int bit,vector<int>&a,vector<vector<int>>&b,vector<vector<vector<int>>>&dp){
+    if(i>=a.size())return 0;
+    if(dp[bit][k][i]!=-1)return dp[bit][k][i];
+    int audi=(k==0)?-1e8:a[i]+f(i+1,k-1,bit,a,b,dp);
+    int player=-1e8;
+    for(int j=0;j<b[0].size();j++){
+        if((bit&(1<<j))==0){
+            player=max(player,b[i][b.size()-1-j]+f(i+1,k,bit|(1<<j),a,b,dp));
+        }
+    }
+    return dp[bit][k][i]=max(player,audi);
+}
+
+void solve(){
+
+    int n,p,k;
+    cin>>n>>p>>k;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    vector<vector<int>>b(n,vector<int>(p));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<p;j++){
+            cin>>b[i][j];
+        }
+    }
+    vector<vector<vector<int>>>dp(128,vector<vector<int>>(k+1,vector<int>(n,-1)));
+    cout<<f(0,k,0,a,b,dp);
+
+
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t=1;
+    while(t--){
+        solve();
+    }
+}
---
+++ 2128D.cpp
@@ -0,0 +1,60 @@
+/*
+   Author: SONIT RAJ
+    created: 22:41:17 03-08-2025
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define int long long
+#define rep(i,a,b) for(int i = a; i<b; i++)
+#define rew(x) for(int i = 0; i<x; i++)
+#define all(x) x.begin(), x.end()
+#ifdef ONLINE_JUDGE
+    #define de(...)
+    #define de2(...)
+#endif
+const ll inf = 2e18 + 5;
+const ll M = 1e9 + 7;
+#define PI 3.141592653589
+
+void solve(){
+
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    vector<int>dp(n,1);
+    int ans=1;
+    for(int i=1;i<n;i++){
+        dp[i]=dp[i-1]+1;
+        if(a[i]<a[i-1]){
+            dp[i]+=i;
+        }
+        ans+=dp[i];
+    }
+    cout<<ans;
+
+
+
+
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+}
