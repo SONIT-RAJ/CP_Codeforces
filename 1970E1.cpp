93f9bf4281c344c9ec730f410ab7436107bcc861
SONIT-RAJ <sonitraj8458@gmail.com>
2025-09-03 23:29:00 +0530

    add new question

--- 1970E1.cpp
+++ 1970E1.cpp
@@ -1,45 +1,36 @@
-/*
-   Author: SONIT RAJ
-    created: 21:09:44 01-09-2025
-*/
-
-
 #include<bits/stdc++.h>
 using namespace std;
 
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx,avx2,fma")
-
-#define ll long long
 #define int long long
-#define rep(i,a,b) for(int i = a; i<b; i++)
-#define rew(x) for(int i = 0; i<x; i++)
-#define all(x) x.begin(), x.end()
-#ifdef ONLINE_JUDGE
-    #define de(...)
-    #define de2(...)
-#endif
-const ll inf = 2e18 + 5;
-const ll M = 1e9 + 7;
-#define PI 3.141592653589
-
-void solve(){
-
-    int n;
-    cin>>n;
-
-
-
-
-}
-
-signed main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int t=1;
-    cin>>t;
-    while(t--){
-        solve();
-        cout<<"\n";
+int m,n;
+long long mod=1e9+7;
+long long recur(int shrt,int day,int cabin,vector<int>& s,vector<int>& l,vector<vector<vector<int>>>& dp){
+    if(!day){
+        return 1;
     }
+    if(dp[shrt][day][cabin]!=-1) return dp[shrt][day][cabin];
+    long long ans=0;
+    if(cabin!=m){
+        if(s[cabin]) ans=(ans+(s[cabin]*recur(1,day,m,s,l,dp)))%mod;
+        if(l[cabin]) ans=(ans+l[cabin]*recur(0,day,m,s,l,dp))%mod;
+    }
+    else{
+        for(int i=0;i<m;i++){
+        if(shrt){
+           if(s[i]) ans=(ans+s[i]*recur(1,day-1,i,s,l,dp))%mod;
+           if(l[i]) ans=(ans+l[i]*recur(0,day-1,i,s,l,dp))%mod;
+        }
+        else if(s[i]) ans=(ans+s[i]*recur(1,day-1,i,s,l,dp))%mod;
+    }
+    }
+    return dp[shrt][day][cabin]=(ans%mod);
+}
+signed main(){
+    cin>>m>>n;
+    vector<vector<vector<int>>> dp(2, vector<vector<int>>(n+1, vector<int>(m+1, -1)));
+        vector<int> s(m,0);
+        vector<int> l(m,0);
+        for(int i=0;i<m;i++) cin>>s[i];
+        for(int i=0;i<m;i++) cin>>l[i];
+        cout<<recur(0,n,0,s,l,dp)<<endl;
 }
