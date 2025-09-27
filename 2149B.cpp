9f2bfb048e672588c4151e508314cad714b0c128
SONIT-RAJ <sonitraj8458@gmail.com>
2025-09-27 13:14:32 +0530

    add new question

---
+++ 2149B.cpp
@@ -0,0 +1,52 @@
+/*
+   Author: SONIT RAJ
+    created: 13:07:53 27-09-2025
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+
+#define ll long long
+#define int long long
+#define rep(i,a,b) for(int i = a; i<b; i++)
+#define rew(x) for(int i = 0; i<x; i++)
+#define all(x) x.begin(), x.end()
+#ifdef ONLINE_JUDGE
+    #define de(...)
+    #define de2(...)
+#endif
+const ll inf = 2e18 + 5;
+const ll M = 1e9 + 7;
+#define PI 3.141592653589
+
+void solve(){
+
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    int ans=0;
+    for(int i=0;i<n-1;i+=2){
+        ans=max(ans,a[i+1]-a[i]);
+    }
+    cout<<ans;
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+}
