fd1894cb616609de6b28be86005a100ca1786c99
SONIT-RAJ <sonitraj8458@gmail.com>
2026-03-17 19:20:32 +0530

    add new question

--- D_Magic_Numbers.cpp
+++ D_Magic_Numbers.cpp
@@ -1,73 +1,89 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define int long long
 
-int dp[2005][2005][2];
-int m,d,mod = 1e9 + 7;
-
-int solve(string&a,int i = 0,int j = 0,int is = 1)
+int main()
 {
-    if (i==(int)a.size()){
-        if (j==0)return 1;
-        return 0;
-    }
+    char data[100];
+    int count = 0;
 
-    if (dp[i][j][is] != -1)return dp[i][j][is];
+    cout << "Enter data: ";
+    cin >> data;
 
-    int ans = 0;
-    int pos = (i + 1)%2;
+    while(data[count] != '\0')
+        count++;
 
-    if (is){
-        for (int p = 0;p<(a[i]-'0');p++){
-            if (pos==0 && p!=d)continue;
-            if (pos==1 && p==d)continue;
+    cout << "Frame transmitted: ";
+    cout << count << data;
 
-            ans += solve(a,i + 1,(10 * j + p)%m,0);
-            ans %= mod;
-        }
-        int p = (a[i] - '0');
-        if (!((pos==0 && p!=d) || (pos==1 && p==d)))
-            ans += solve(a,i + 1,(10 * j + (a[i] - '0'))%m,1);
-    }
-    else{
-        for(int p = 0;p<=9;p++){
-            if (pos==0 && p!=d)continue;
-            if (pos==1 && p==d)continue;
-
-            ans += solve(a,i + 1,(10 * j + p)%m,0);
-            ans %= mod;
-        }
-    }
-
-    return dp[i][j][is] = ans;
-}
-bool is(string&a)
-{
-    int rem = 0;
-    for (int i = 0;i<(int)a.size();i++){
-        int pos = (i + 1)%2;
-        if (pos==0 && (a[i] - '0') != d)return false;
-        if (pos==1 && (a[i] - '0') == d)return false;
-        rem = (10 * rem + (a[i] - '0'))%m;
-    }
-    return rem==0;
-}
-int32_t main()
-{
-    cin>>m>>d;
-    int aa,bb;
-    cin>>aa>>bb;
-    string b=to_string(bb);
-    string a=to_string(aa-1);
-    memset(dp,-1,sizeof(dp));
-
-    int ans = solve(b);
-    memset(dp,-1,sizeof(dp));
-    ans -= solve(a);
-    ans %= mod;
-    //if (is(a))ans++;
-    ans %= mod;
-    if (ans<0)ans += mod;
-    cout<<ans;
     return 0;
 }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     char data[100];
+//     int i;
+
+//     cout << "Enter data: ";
+//     cin >> data;
+
+//     cout << "Frame after character stuffing:\n";
+//     cout << "DLE STX ";
+
+//     for(i=0; data[i] != '\0'; i++)
+//     {
+//         if(data[i] == 'D')   // assuming D represents DLE
+//             cout << "DD";
+//         else
+//             cout << data[i];
+//     }
+
+//     cout << " DLE ETX";
+
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int data[100], stuffed[200];
+//     int n, i, j = 0, count = 0;
+
+//     cout << "Enter number of bits: ";
+//     cin >> n;
+
+//     cout << "Enter bits:\n";
+//     for(i=0;i<n;i++)
+//         cin >> data[i];
+
+//     for(i=0;i<n;i++)
+//     {
+//         stuffed[j] = data[i];
+//         j++;
+
+//         if(data[i] == 1)
+//         {
+//             count++;
+//             if(count == 5)
+//             {
+//                 stuffed[j] = 0;
+//                 j++;
+//                 count = 0;
+//             }
+//         }
+//         else
+//             count = 0;
+//     }
+
+//     cout << "After bit stuffing:\n";
+
+//     for(i=0;i<j;i++)
+//         cout << stuffed[i];
+
+//     return 0;
+// }
